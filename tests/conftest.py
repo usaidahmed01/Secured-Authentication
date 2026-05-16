86935b4ec8bba6a5368941bba6ededbfd73ab91e
usaidahmed01 <usaid423@gmail.com>
2026-05-16 21:56:54 +0500

    Fixing CI/CD

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,52 @@
+# import pytest_asyncio
+# from httpx import ASGITransport, AsyncClient
+
+# from app.db.redis import close_redis, connect_redis
+# from app.db.session import engine
+# from app.main import app
+
+
+# @pytest_asyncio.fixture
+# async def client():
+#     await connect_redis()
+
+#     transport = ASGITransport(app=app)
+
+#     async with AsyncClient(
+#         transport=transport,
+#         base_url="http://testserver",
+#     ) as test_client:
+#         yield test_client
+
+#     await close_redis()
+#     await engine.dispose()
+
+
+
+
+
+
+
+
+
+
+
 import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 
+from app.db.base import Base
 from app.db.redis import close_redis, connect_redis
 from app.db.session import engine
 from app.main import app
+from app.models import User  # noqa: F401
 
 
 @pytest_asyncio.fixture
 async def client():
     await connect_redis()
+
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
 
     transport = ASGITransport(app=app)
 
